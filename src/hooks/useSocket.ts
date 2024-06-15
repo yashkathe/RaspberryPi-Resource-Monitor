c18af786091eee35ea53d8ae13161763dc801b41
yashkathe <yashkathe123@gmail.com>
2024-06-15 07:58:02 +0000

    Update frontend to output generated at d4772ff

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import socketIOClient from 'socket.io-client'
+
+const backendServer: string = import.meta.env.VITE_BACKEND_SERVER as string
+
+export const useSocket = <T>(eventName: string, socketUrl: string = backendServer) => {
+    const [data, setData] = useState<T[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const socket = socketIOClient(socketUrl);
+
+        // connect to a socket 
+        socket.on(eventName, (data: T[]) => {
+            setData(data);
+            setIsLoading(false);
+        });
+
+        // handle socket disconnects
+        socket.on("disconnect", () => {
+            setIsLoading(true);
+        });
+
+        // prevent memory leaks
+        return () => {
+            setIsLoading(true);
+            socket.disconnect();
+        };
+    }, [socketUrl, eventName]);
+
+    return { data, isLoading };
+};
--- src/sockets/CpuUtil.tsx
+++ src/sockets/CpuUtil.tsx
@@ -1,38 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import socketIOClient from 'socket.io-client'
-
+import React from 'react'
 import BarChartGraph from '../graphs/BarChartGraph';
+
 import Spinner from '../shared/Spinner';
+import { useSocket } from '../hooks/useSocket';
 import { isCpuUtil } from '../types/types';
 
 import classes from './shared.module.css'
 
 const CpuUtil: React.FC = () => {
 
-    const [data, setData] = useState<isCpuUtil[]>([])
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-
-    // connect the socket
-    useEffect(() => {
-
-        const socket = socketIOClient("http://192.167.0.119:4001/");
-
-        socket.on("cpuUsage", (data: isCpuUtil[]) => {
-            setData(data);
-            setIsLoading(false)
-        });
-
-        socket.on("disconnect", () => {
-            setIsLoading(true)
-        })
-
-        // Clean up the socket connection when the component unmounts
-        return () => {
-            setIsLoading(true)
-            socket.disconnect();
-        };
-
-    }, [])
+    const {data, isLoading} = useSocket<isCpuUtil>("cpuUsage")
 
     return (
         <div className={classes.parent}>
